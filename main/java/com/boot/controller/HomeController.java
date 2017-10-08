987ea26d4f573e14c6675c6fcc9c03b82b7401de
Saurav Kumar <thekumar101@gmail.com>
2017-10-08 17:39:33 +0530

    added all files

---
+++ main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Pass-through";
+    }
+
+    @RequestMapping("/event")
+    public String event() {
+        return "Pass-through.event";
+    }
+}
